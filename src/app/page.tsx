f6e2b64df5bb0998e15633cbacb43fefaf1fbf32
Theo Browne <me@t3.gg>
2024-04-01 23:06:48 -0700

    Add padding back on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,8 +9,8 @@ async function Images() {
   const images = await getMyImages();
 
   return (
-    <div className="flex flex-wrap justify-center gap-4">
-      {[...images, ...images, ...images].map((image) => (
+    <div className="flex flex-wrap justify-center gap-4 p-4">
+      {images.map((image) => (
         <div key={image.id} className="flex h-48 w-48 flex-col">
           <Link href={`/img/${image.id}`}>
             <Image
