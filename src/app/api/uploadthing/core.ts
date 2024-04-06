86d4b2dbcb008f3312782b5fb7ea77cdc4db57d0
Theo Browne <me@t3.gg>
2024-04-06 16:52:29 -0700

    Clerk middleware usage

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,4 +1,4 @@
-import { auth } from "@clerk/nextjs/server";
+import { auth, clerkClient } from "@clerk/nextjs/server";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { UploadThingError } from "uploadthing/server";
 import { db } from "~/server/db";
@@ -12,6 +12,11 @@ export const ourFileRouter = {
     .middleware(async ({ req }) => {
       const user = auth();
       if (!user.userId) throw new UploadThingError("Unauthorized");
+
+      const fullUserData = await clerkClient.users.getUser(user.userId);
+
+      if (fullUserData?.privateMetadata?.["can-upload"] !== true)
+        throw new UploadThingError("User Does Not Have Upload Permissions");
 
       const { success } = await ratelimit.limit(user.userId);
       if (!success) throw new UploadThingError("Ratelimited");
